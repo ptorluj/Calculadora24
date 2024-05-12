aeadff57a89514e3d32b311f2d811ddab80b92a3
ptorluj <ptorluj539@g.educaand.es>
2024-05-12 21:52:37 +0200

    Add files via upload

---
+++ PruebaCalculadora_24_LocalBase/src/pruebacalculadora/Calculando.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebacalculadora;
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculando {
+
+    public double add(double number1, double number2){
+    return 0;
+    } 
+    
+    public double subtract(double number1, double number2){ 
+   return 0;
+    } 
+    
+    public double multiply(double number1, double number2) { 
+    return 0;
+    } 
+
+    public double divide(double number1, double number2){
+       return 0;
+    }
+       
+    public double resto(double dividendo, double divisor){
+       return 0;   
+    }
+    public double potencia(double base, double esponente){
+       return 0;   
+    }
+    public double factorial(double number1){
+       return 0;   
+    }
+
+
+
+
+}
---
+++ PruebaCalculadora_24_LocalBase/test/pruebacalculadora/CalculandoTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebacalculadora;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author usuario
+ */
+public class CalculandoTest {
+     
+    public CalculandoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class Calculando.
+     */
+    @Test
+    public void testAdd() {
+       
+        
+    }
+
+    /**
+     * Test of subtract method, of class Calculando.
+     */
+    @Test
+    public void testSubtract() {
+      
+    }
+
+    /**
+     * Test of multiply method, of class Calculando.
+     */
+    @Test
+    public void testMultiply() {
+       
+    }
+
+    /**
+     * Test of divide method, of class Calculando.
+     */
+    @Test//(expResult=ArithmeticException.class)
+    public void testDivide() {
+        
+    }
+    
+    @Test//(expResult=ArithmeticException.class)
+    public void testResto() {
+        
+    }
+    
+    @Test//(expResult=ArithmeticException.class)
+    public void testPotencia() {
+        
+    }
+    
+    @Test//(expResult=ArithmeticException.class)
+    public void testFactorial() {
+        
+    }
+    
+}
